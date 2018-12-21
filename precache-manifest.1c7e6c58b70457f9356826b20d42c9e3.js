4ae4cc4db61439d0a9825a88a04a94f8d735f9b0
mwomack <mikewomack2000@yahoo.com>
2018-12-20 18:52:00 -0800

    Updates

---
+++ precache-manifest.1c7e6c58b70457f9356826b20d42c9e3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b8253412466ef0649bb1f02c543da23",
+    "url": "/Clicky-Game-Southpark/static/media/ike.1b825341.png"
+  },
+  {
+    "revision": "d63fc1a71a16515c5112",
+    "url": "/Clicky-Game-Southpark/static/css/main.5f6f2f9b.chunk.css"
+  },
+  {
+    "revision": "6a558196190899cd1d26",
+    "url": "/Clicky-Game-Southpark/static/js/1.6a558196.chunk.js"
+  },
+  {
+    "revision": "23aac79dd40fa00d2c68",
+    "url": "/Clicky-Game-Southpark/static/js/runtime~main.23aac79d.js"
+  },
+  {
+    "revision": "480cec6502f7e9600f65e9c11c78be58",
+    "url": "/Clicky-Game-Southpark/static/media/chef.480cec65.png"
+  },
+  {
+    "revision": "58d9fe273939b4fa855a93403026dba3",
+    "url": "/Clicky-Game-Southpark/static/media/butters.58d9fe27.png"
+  },
+  {
+    "revision": "fa49c424a1d2c14f28f4e55ec3e52aea",
+    "url": "/Clicky-Game-Southpark/static/media/cartmen.fa49c424.png"
+  },
+  {
+    "revision": "e0fa4cab1d3172672568ac389cc0d27f",
+    "url": "/Clicky-Game-Southpark/static/media/garrison.e0fa4cab.png"
+  },
+  {
+    "revision": "d63fc1a71a16515c5112",
+    "url": "/Clicky-Game-Southpark/static/js/main.d63fc1a7.chunk.js"
+  },
+  {
+    "revision": "e79069616f21c44dfeda106e8341e89d",
+    "url": "/Clicky-Game-Southpark/static/media/jimmy.e7906961.png"
+  },
+  {
+    "revision": "b996a6781320a1bbae6636109e92ea3c",
+    "url": "/Clicky-Game-Southpark/static/media/kenny.b996a678.png"
+  },
+  {
+    "revision": "6fc3e8a4b0888f447ff6700fe32b4073",
+    "url": "/Clicky-Game-Southpark/static/media/kyle.6fc3e8a4.png"
+  },
+  {
+    "revision": "b09d28c51bed72efb3e2a86843587864",
+    "url": "/Clicky-Game-Southpark/static/media/nathan.b09d28c5.png"
+  },
+  {
+    "revision": "c0290148021af8807709a471033c82ba",
+    "url": "/Clicky-Game-Southpark/static/media/PC.c0290148.png"
+  },
+  {
+    "revision": "e25ed94c7d102818cf56dff5a58ea0c7",
+    "url": "/Clicky-Game-Southpark/static/media/stan.e25ed94c.png"
+  },
+  {
+    "revision": "f021edc0239e020b8c633016648c426e",
+    "url": "/Clicky-Game-Southpark/static/media/token.f021edc0.png"
+  },
+  {
+    "revision": "80346158505f6ab893ff98964a69492a",
+    "url": "/Clicky-Game-Southpark/index.html"
+  }
+];
